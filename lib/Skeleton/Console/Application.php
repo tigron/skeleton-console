0ffa4ba54e200af59e04fa14385d12ce0c7c408b
Tigron <tigron@bookkeeping.tigron.net>
2015-09-22 16:18:05 +0200

    search in every Skeleton-* package for console commands

--- lib/Skeleton/Console/Application.php
+++ lib/Skeleton/Console/Application.php
@@ -10,7 +10,7 @@
 
 namespace Skeleton\Console;
 
-class Application extends Symfony\Component\Console\Application {
+class Application extends \Symfony\Component\Console\Application {
 
 	/**
 	 * Constructor
@@ -27,7 +27,44 @@ class Application extends Symfony\Component\Console\Application {
 	 * @access public
 	 */
 	public function add_commands() {
+		/**
+		 * Search for other Skeleton packages installed
+		 */
+		$installed = file_get_contents(__DIR__ . '/../../../../../composer/installed.json');
+		$installed = json_decode($installed);
 
+		$packages = [];
+		foreach ($installed as $install) {
+			$package = $install->name;
+			list($vendor, $name) = explode('/', $package);
+			if ($vendor != 'tigron') {
+				continue;
+			}
+			$packages[] = $name;
+		}
+
+		/**
+		 * Check in every Skeleton package for commands
+		 */
+		foreach ($packages as $package) {
+			if (!file_exists(__DIR__ . '/../../../../' . $package . '/console')) {
+				continue;
+			}
+
+			$files = scandir(__DIR__ . '/../../../../' . $package . '/console');
+
+			foreach ($files as $file) {
+				if ($file[0] == '.') {
+					continue;
+				}
+				require_once __DIR__ . '/../../../../' . $package . '/console/' . $file;
+
+				$file = str_replace('.php', '', $file);
+
+				$classname = '\Skeleton\Console\Command\\' . ucfirst(str_replace('skeleton-', '', $package)) . '_' . ucfirst($file);
+				$this->add(new $classname);
+			}
+		}
 	}
 
 }
--- lib/Skeleton/Console/Command.php
+++ lib/Skeleton/Console/Command.php
@@ -2,7 +2,6 @@
 /**
  * Command Class
  *
- *
  * @author Gerry Demaret <gerry@tigron.be>
  * @author Christophe Gosiau <christophe@tigron.be>
  * @author David Vandemaele <david@tigron.be>
@@ -10,13 +9,12 @@
 
 namespace Skeleton\Console;
 
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class Command extends use Symfony\Component\Console\Command\Command {
+class Command extends \Symfony\Component\Console\Command\Command {
 
 	/**
 	 * Configure method
